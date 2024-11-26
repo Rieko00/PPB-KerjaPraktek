9d8090a578a895d68567c63a9f160b3f7fa18e17
Rieko00 <ariekbudianto@gmail.com>
2024-11-26 14:26:20 +0700

    Edit README

--- app/Models/Pengajuan_kp.php
+++ app/Models/Pengajuan_kp.php
@@ -7,15 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Pengajuan_kp extends Model
 {
     protected $fillable = [
-        'nim',
-        'nama',
-        'judul',
-        'lembaga',
-        'alamat',
-        'pimpinan',
-        'no_telp',
-        'fax',
-        'created_at',
-        'updated_at',
+        'tanggal_pengajuan',
+        'status_pengajuan',
+
     ];
 }
